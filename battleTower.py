9b1370a275db83a756f93568894ac9f4e2062a40
jimmyglasscock <jamglass42@gmail.com>
2019-08-16 11:00:23 -0600

    added helpful comments

--- battleTower.py
+++ battleTower.py
@@ -1,9 +1,13 @@
+#rename me
 class Po:
 	# the constructor will generate the stats
 	# takes in moves as an array
 	# types will be an array as well
 	def __init__(self, num, name, attack, defense, specAttack, specDefense, speed, HP, MaxHP, types, moves):
 		#generate stats here
+
+		# HP = (((2 * baseHP + IV + (EV/4)) * level)/100) + level + 10
+		# Other Stat = ((((2 * baseStat + IV + (EV/4)) * level)/100) + 5) * nature
 
 		self.num =num
 		self.name = name
