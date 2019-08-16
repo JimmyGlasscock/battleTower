f7b8ef4ea58a355d76d217f0fd6ea7e3a5c1e15b
jimmyglasscock <jamglass42@gmail.com>
2019-08-16 11:15:24 -0600

    added stat generation! yay!

--- battleTower.py
+++ battleTower.py
@@ -3,24 +3,35 @@ class Po:
 	# the constructor will generate the stats
 	# takes in moves as an array
 	# types will be an array as well
-	def __init__(self, num, name, attack, defense, specAttack, specDefense, speed, HP, MaxHP, types, moves):
+	def __init__(self, num, name, level, attack, defense, specAttack, specDefense, speed, HP, types, moves):
 		#generate stats here
 
-		# HP = (((2 * baseHP + IV + (EV/4)) * level)/100) + level + 10
-		# Other Stat = ((((2 * baseStat + IV + (EV/4)) * level)/100) + 5) * nature
+		#until EV, IV and nature support is added, they will be these values:
+		IV = 0
+		EV = 0
+		nature = 1
 
 		self.num =num
 		self.name = name
-		self.attack = attack
-		self.defense = defense
-		self.specAttack = specAttack
-		self.specDefense = specDefense
-		self.speed = speed
-		self.HP = HP
-		self.MaxHP = MaxHP
-
+		self.level = level
+		self.attack = generateStat(attack, IV, EV, level, nature)
+		self.defense = generateStat(defense, IV, EV, level, nature)
+		self.specAttack = generateStat(specAttack, IV, EV, level, nature)
+		self.specDefense = generateStat(specDefense, IV, EV, level, nature)
+		self.speed = generateStat(speed, IV, EV, level, nature)
+		self.MaxHP = generateHP(HP, IV, EV, level)
+		self.HP = self.MaxHP
 		self.types = types
 		self.moves = moves
+
+	# until support for this is added, ev & iv values will be 0, nature will be 1
+	def generateStat(stat, IV, EV, level, nature):
+		newStat = ((((2 * stat + IV + (EV/4)) * level)/100) + 5) * nature;
+		return newStat;
+
+	def generateHP(HP, IV, EV, level):
+		newHP = (((2 * HP + IV + (EV/4)) * level)/100) + level + 10;
+		return newHP;
 
 class Move: 
 	# takes in physical as a boolean to determine physical/special
